d7461e5dbb540306ae44883ff3d7c725f16641a8
Abdul Rehman Khan <arkhan@Abduls-MacBook-Air.local>
2025-07-23 11:58:21 +0530

    Fix: remove require

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const port = process.env.PORT || 3450;
 const app = express();
 import viewsRouter from "./routes/views.js";
 import apiRouter from "./routes/api.js";
-const cron = require("node-cron");
+import cron from "node-cron";
+import { saveNewsToDB } from "./database/index.js";
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
